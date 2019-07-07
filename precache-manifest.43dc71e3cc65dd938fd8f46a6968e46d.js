a8af840bc06aba1c6d81686072fd3c93b87fabbf
Rishabh Agarwal <rishabh33agarwal@gmail.com>
2019-07-07 16:55:46 +0530

    Updates

---
+++ precache-manifest.43dc71e3cc65dd938fd8f46a6968e46d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da2b33d79832a467d8981f6a3760661",
+    "url": "/Monster/index.html"
+  },
+  {
+    "revision": "4b9feab48ae50c90033a",
+    "url": "/Monster/static/css/main.77eb329f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4b9feab48ae50c90033a",
+    "url": "/Monster/static/js/main.8d6e0a99.chunk.js"
+  },
+  {
+    "revision": "cded30659477d4f2a55b",
+    "url": "/Monster/static/js/runtime~main.09aeed93.js"
+  }
+]);
